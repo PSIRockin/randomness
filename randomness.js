2cb56a0b0b24d36dff8b9ad8e23673f851e61e4f
PSIRockin <jonjrn@gmail.com>
2018-07-12 09:42:54 -0400

    Finished the first section

---
+++ randomness.js
@@ -0,0 +1,35 @@
+function randomNumber () {
+    const deci = Math.random();
+    const decimal = deci + Math.random();
+    const times10 = decimal * 10
+    const final = Math.floor (times10)
+    console.log (final)
+}
+
+randomNumber ();
+
+
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
